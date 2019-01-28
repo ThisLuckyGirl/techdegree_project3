32fb48a1cedaf6a4569c007d9d78d63aa19d72ac
Christina <thisluckygrrl@gmail.com>
2019-01-27 21:58:35 -0800

    Add files via upload
    working on <li> on index.php

--- index.php
+++ index.php
@@ -1,10 +1,30 @@
-<?php include("inc/header.php"); ?>
+<?php include("inc/header.php");
+//require('inc/journal.db');
+
+try {
+    $results = $db->query('SELECT * FROM entries');
+} catch(Exception $e) {
+    echo $e->getMessage();
+    die();
+}
+
+$journalEntries = $results->fetchAll(PDO::FETCH_ASSOC);
+
+?>
 
 <!DOCTYPE html>
 <html>
         <section>
             <div class="container">
                 <div class="entry-list">
+                  <ol>
+                    <?php
+                        foreach($journalEntries as $journalEntry) {
+                            echo '<li><a href="detail.php">' .
+                            $journalEntry[$title] . '</li>';
+                        }
+                    ?>
+                  <!--
                     <article>
                         <h2><a href="detail.php">The best day I’ve ever had</a></h2>
                         <time datetime="2016-01-31">January 31, 2016</time>
@@ -21,6 +41,7 @@
                         <h2><a href="detail_4.php">Dude, where’s my car?</a></h2>
                         <time datetime="2016-01-31">January 31, 2016</time>
                     </article>
+                  -->
                 </div>
             </div>
         </section>
